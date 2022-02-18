e8d1e92f3150602806afdda16a4085c58427e4a6
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-02-18 16:42:24 +0100

    Added example of connection with ZUS service

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zus3.ServiceReference1;
+
+namespace Zus3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new NawsUslugiClient();
+
+            var bytes = new byte[100];
+            uint result = 0;
+
+            client.CheckTransmision(bytes, 100, ref bytes, ref result);
+        }
+    }
+}
